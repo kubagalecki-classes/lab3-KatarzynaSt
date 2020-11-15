573a4c0376d7e02861d251120f35ddf172137553
KatarzynaSt <katarzyna.steczniewska.stud@pw.edu.pl>
2020-11-15 20:25:47 +0000

    wersja 5

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -3,25 +3,24 @@
 #include "Stocznia.hpp"
 
 unsigned int transportujFlota(unsigned int towar)
-{int liczba;
+{unsigned int PrzewiezionyTowar=0;
 int zagl;
-  if (towar=0)
-      return 0;
-  else   {
-
+  if(towar==0)
+    {return 0;}
+  else {
   int i=0;
-  for (i; i<towar)
+  for (i<towar)
   
   Stocznia stocznia{};
   Statek* s1 = stocznia();
 
-if (Zaglowiec* zaglowiec = dynamic_cast< Zaglowiec* >(s1)) {zagl++;}
-else {}
+if (dynamic_cast< Zaglowiec* >(s1)) {zagl++;}
+else {
   
-  liczba=statek->transportuj();
-  i+=liczba;
+  PrzewiezionyTowar=s1->transportuj();
+  i=i+PrzewiezionyTowar;
   delete s1;
-}
-
+  }
+  }
 return zagl;
 } 
