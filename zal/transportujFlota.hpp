6ff12b9312ac0c2f2e4d409de6cca67dc7916a5b
KatarzynaSt <katarzyna.steczniewska.stud@pw.edu.pl>
2020-11-16 10:45:17 +0000

    wersja 7

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,23 +4,25 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {unsigned int PrzewiezionyTowar=0;
-int zagl;
+int zagl=0;
   if(towar==0)
-    {return 0;}
+     {return 0;}
   else {
-  int i=0;
-  for (is<towar)
-  
-  Stocznia stocznia{};
-  Statek* s1 = stocznia();
+      int i=0;
+      for (i<towar)
+        {
+         Stocznia stocznia{};
+         Statek* s1 = stocznia();
 
-if (dynamic_cast< Zaglowiec* >(s1)) {zagl++;}
-else {
-  
-  PrzewiezionyTowar=s1->transportuj();
-  i=i+PrzewiezionyTowar;
-  delete s1;
-  }
-  }
+            if (Zaglowiec* wziety= dynamic_cast< Zaglowiec* >(s1))
+                   {zagl++;}
+            else
+             {
+                PrzewiezionyTowar=s1->transportuj();
+                i=i+PrzewiezionyTowar;
+                delete s1;
+             }
+        }
+       }
 return zagl;
 } 
