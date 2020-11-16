4722d03335cf7175551842624030f54165638ca7
KatarzynaSt <katarzyna.steczniewska.stud@pw.edu.pl>
2020-11-16 13:25:22 +0000

    wersja 9

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,25 +4,33 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-  int PrzewiezionyTowar=0;
-int zagl=0;
-  if(towar=0)
-     {return 0;}
-  else {
-      int i=0;
-      for (i<towar)
+
+    std::cout << towar << std::endl;
+    int PrzewiezionyTowar = 0;
+    int zagl = 0;
+    if (towar == 0)
+    {
+        return 0;
+    }
+    else {
+        int i = 0;
+        while (i < towar)
         {
-         Stocznia stocznia{};
-         Statek* s1 = stocznia();
-         PrzewiezionyTowar=s1->transportuj();
-         i=i+PrzewiezionyTowar;
-        
-            if (s1= dynamic_cast< Zaglowiec* >(s1))
-                   {zagl++;}
+            Stocznia stocznia{};
+            Statek* s1 = stocznia();
+            PrzewiezionyTowar = s1->transportuj();
+            i = i + PrzewiezionyTowar;
+
+            if (s1 == dynamic_cast<Zaglowiec*>(s1))
+            {
+                zagl++;
+            }
             else
-             {  }
-         delete s1;
-       }
-       }
-return zagl;
-} 
+            {
+            }
+            delete s1;
+        }
+    }
+    std::cout << zagl << std::endl;
+    return zagl;
+}
